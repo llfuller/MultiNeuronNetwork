75b1d539624bdc436b74f114ada9299a9661b317
Lawson Fuller <llfuller@users.noreply.github.com>
2023-01-28 22:05:28 -0800

    Three Neuron Network gives reasonable results

--- Three_Neuron_Network.py
+++ Three_Neuron_Network.py
@@ -13,42 +13,38 @@ def g(V,A,B):
     :param B: Length scale of tanh (mV);  dim=(gating variables,)
     :return: g (muS); dim=(# neurons, # gating variables)
     """
-    g = 0.5*(1.0+np.tanh(np.divide(float(V[:,None]-A[None,:]),float(B[None,:]))))
-    print(f"dim(g)={g.shape}")
+    g = 0.5*(1.0+np.tanh(np.divide(V[:,None]-A[None,:],B[None,:])))
     return g
 
 def S0(V_presyn,V0,dV0):
-    # TODO: Calculate this dimension first
     """
     Asymptotic value of synapse variable (analogous to gating variable).
 
-    :param V_presyn: Voltage (mV) of presynaptic neuron
-    :param V0: Inflection voltage (where S0(V)=0) (mV)
-    :param dV0: Length scale of tanh (mV)
-    :return: S0 (unitless (?)) TODO: Check return dimension
+    :param V_presyn: Voltage (mV) of presynaptic neuron; dim = (# neurons)
+    :param V0: Inflection voltage (where S0(V)=0) (mV) of presynaptic neuron; (scalar)
+    :param dV0: Length scale of tanh (mV) for presynaptic neuron; (scalar)
+    :return: S0; dim = (# neurons)
     """
-    S0 = 0.5*(1.0+np.tanh(np.divide(float(V_presyn[:,None]-V0[None,:]),float(dV0[None,:]))))
-    print(f"dim(S0)={S0.shape}")
+    S0 = 0.5*(1.0+np.tanh(np.divide(V_presyn-V0,dV0)))
     return S0
 
 def tau(V,t0,t1,A,B):
     """
     Time constant of gating variable ODEs.
 
-    :param V: Voltage (mV)
+    :param V: Voltage (mV); dim=(# neurons)
     :param t0: Constant (ms)
     :param t1: Constant (ms)
-    :param A: Location of g(V)=0 (mV)
-    :param B: Length scale of tanh (mV)
-    :return: tau (Voltage-dependent time constant) (ms) TODO: Check return dimension
+    :param A: Location of g(V)=0 (mV);   dim=(gating variables,)
+    :param B: Length scale of tanh (mV);   dim=(gating variables,)
+    :return: tau (Voltage-dependent time constant) (ms); dim=(# neurons, # gating variables)
     """
-    tau = t0+t1*(1.0-np.power(np.tanh(np.divide(float(V[:,None]-A[None,:]),float(B[None,:]))),2))
-    print(f"dim(tau)={tau.shape}")
+    epsilon = 0.0000000000000001
+    tau = t0+t1*(1.0-np.power(np.tanh(np.divide(V[:,None]-A[None,:],B[None,:]+epsilon)),2))
     return tau
 
 
 def calc_dAdt(V,A,vA,dvA,tA0,tA1):
-    # TODO: Need tau() dimension before this dim can be calculated
     """
     Time derivative of ion channel gating variable "A" (example: m, h, n)
 
@@ -58,36 +54,35 @@ def calc_dAdt(V,A,vA,dvA,tA0,tA1):
     :param dvA: Length scale of tanh (mV)
     :param tA0: Constant (ms)
     :param tA1: Constant (ms)
-    :return: dAdt (unitless) TODO: Check return dimension
+    :return: dAdt; dim=(# neurons, # gating variables)
     """
-    dAdt = np.divide((g(V,vA,dvA) - A), tau(V,tA0,tA1,vA,dvA))
-    print(f"dim(dAdt)={dAdt.shape}")
+    epsilon = 0.0000000000000001
+    dAdt = np.divide(g(V,vA,dvA) - A, tau(V,tA0,tA1,vA,dvA) +epsilon)
     return dAdt
 
 def calc_dSdt(V_presyn, S, V0, dV0, tau_syn, S_syn):
-    # TODO: Need S0 dimension before this dim can be calculated
     """
     Time derivative of synapse "gating variable" "S" (analogous to ion channel gating variables: m, h, n)
 
     :param V_presyn: Presynaptic neuron voltage (mV); dim = (# neurons)
     :param S: Synapse variable (unitless); # dim index =(postsyn neuron index, presyn neuron index)
-    :param V0: Inflection voltage (where S0(V)=0) (mV)
-    :param dV0: Length scale of tanh (mV)
-    :param tau_syn: tau_1 = excitatory, tau_2 = inhibitory; dim = (2,)
-    :param S_syn: S_1=excitatory, S_2 = inhibitory; dim=(2,)
-    :return: dSdt (unitless) TODO: Check return dimension
+    :param V0: Inflection voltage (where S0(V)=0) (mV) of presynaptic neuron; (scalar)
+    :param dV0: Length scale of tanh (mV) of presynaptic neuron; (scalar)
+    :param tau_syn: tau_1 = excitatory, tau_2 = inhibitory; dim = (postsyn neuron index, presyn neuron index)
+    :param S_syn: S_1=excitatory, S_2 = inhibitory; dim = (postsyn neuron index, presyn neuron index)
+    :return: dSdt; dim index =(postsyn neuron index, presyn neuron index)
     """
-    S0_value = S0(V_presyn,V0,dV0)
-    dSdt = np.divide( (S0_value-S), (np.multiply(tau_syn,(S_syn-S0_value))) )
-    print(f"dim(dSdt)={dSdt.shape}")
+    epsilon = 0.0000000000000001
+    S0_value = S0(V_presyn,V0,dV0) # dim = (# postsynaptic neurons)
+    dSdt = np.divide( (S0_value[None,:]-S), (np.multiply(tau_syn,(S_syn-S0_value[None,:]))) +epsilon)
     return dSdt
 
 
-def calc_dVdt(V, A_modulation, g_internal_gating, E_rev_gate_matrix):
+def calc_dVdt(V, A_modulation, g_internal_gating, E_rev_gate_matrix, S_modulation, E_rev_syn_matrix, g_syn):
     """
     Voltage calculation. Accepts vectors and matrices. See documentation for necessary dimension.
 
-    :param V: V (mV) dim: (# neurons,)
+    :param V: V (mV) dim: (# neurons,); Note: if using V[None,:], this represents PREsynaptic neurons
     :param A_modulation: dim: (# neurons, # ion channels)
                             [[m1,   h1,    n1]
                             [m2,    h2,    n2]
@@ -95,11 +90,81 @@ def calc_dVdt(V, A_modulation, g_internal_gating, E_rev_gate_matrix):
                             [mLast, hLast, nLast]]
     :param g_internal_gating: dim: (# ion channels)
     :param E_rev_gate_matrix: dim: (# neurons, # ion channels)
-    :param E_rev_syn_matrix: dim: (# neurons, # neurons)
+    :param S_modulation: dim: (# postsyn neurons, # presyn neurons)
+                        [[S11,   S12,       S13]
+                        [S21,    S22,       S23]
+                         ....
+                        [SLast1, SLast2, SLast3]]
+    :param E_rev_syn_matrix: dim: (# postsyn neurons, # presyn neurons)
+    :param g_syn: dim: (# postsyn neurons, # presyn neurons)
     :return: dVdt: dim (# neurons)
     """
-    dVdt = np.dot(np.multiply(A_modulation, E_rev_gate_matrix[0] - V), g_internal_gating)
-    return dVdt
+    dVdt_internal = np.dot(np.multiply(A_modulation, E_rev_gate_matrix - V[:,None]), g_internal_gating)
+    # dVdt_internal = np.sum(np.multiply(np.multiply(A_modulation, E_rev_gate_matrix - V[:,None]), g_internal_gating[None,:]),axis=1)
+    # test_multiplication_a =  np.multiply(S_modulation, E_rev_syn_matrix)
+    # test_multiplication_b =  np.multiply(S_modulation, V[None,:])
+    #
+    # test_multiplication =  np.multiply(S_modulation, E_rev_syn_matrix - V[None,:])
+    # test_2_multi = np.multiply(g_syn, test_multiplication)
+    dVdt_synapse = np.sum(np.multiply(g_syn, np.multiply(S_modulation, E_rev_syn_matrix - V[None,:])),axis=1)
+    return dVdt_internal + dVdt_synapse # TODO: Remove the zero from this line
+
+def dStatedt(t, state, params):
+    """
+    Run this inside ODEInt or SciPy's integrate. Contains vectorized ODEs for 3 NaKL neurons.
+    :param t:
+    :param state:
+    :param params:
+    :return:
+    """
+    # V1, V2, V3 = state[0:3]
+    # m1, m2, m2 = state[3:6]
+    # h1, h2, h2 = state[6:9]
+    # n1, n2, n2 = state[9:12]
+    vA = params[0]["vA"]
+    dvA = params[0]["dvA"]
+    tA0 = params[0]["tA0"]
+    tA1 = params[0]["tA1"]
+    tau_syn_arr = params[0]["tau_syn_arr"] # dim = (postsyn neuron index, presyn neuron index)
+    S_syn_arr = params[0]["S_syn_arr"] # dim = (postsyn neuron index, presyn neuron index)
+    g_internal_gating = params[0]["g_internal_gating"]
+    E_rev_gate_matrix = params[0]["E_rev_gate_matrix"]
+    E_rev_syn_matrix = params[0]["E_rev_syn_matrix"]
+    g_syn = params[0]["g_synapse_gating"]
+    I_ext = params[0]["I_ext"]
+
+    # Prepare for matrix multiplication later in function
+    V = state[0:3]
+    m = state[3:6]
+    h = state[6:9]
+    n = state[9:12]
+    # S = state[12:16] # S12, S23, S31, S32 = state[12:16]
+    S12, S23, S31, S32 = state[12:16]
+    S_arr = np.array([[0, S12, 0],
+                      [0, 0, S23],
+                      [S31, S32, 0]]) # dim index =(postsyn neuron index, presyn neuron index)
+
+    A = np.array([m,
+                  h,
+                  n]).transpose() # dim=(# neurons, # gating variables)
+    m3h = np.multiply(np.power(m, 3), h)
+    A_modulation = np.array([m3h,
+                            np.power(n,4),
+                            np.ones(V.size)]).transpose() # dim(3,3) or (# neurons, # ion channels)
+
+    # Perform matrix multiplications
+    # Voltages: dim(dVdt)=(# neurons,)
+    dVdt = calc_dVdt(V, A_modulation, g_internal_gating, E_rev_gate_matrix,
+                     S_arr, E_rev_syn_matrix, g_syn) + I_ext
+    # Gating variables: dim(dAdt)=(#gating variables,# neurons)
+    dAdt = calc_dAdt(V,A,vA,dvA,tA0,tA1).transpose()
+    # Synapses: dim(dSdt)=(# neurons,# neurons); dim index =(postsyn neuron index, presyn neuron index)
+    dSdt = calc_dSdt(V, S_arr, V0, dV0, tau_syn_arr, S_syn_arr)
+    dStatedt = np.empty((16))
+    dStatedt[0:3] = dVdt
+    dStatedt[3:12] = np.ravel(dAdt)
+    dStatedt[12:16] = np.array([dSdt[0][1],dSdt[1][2],dSdt[2][0],dSdt[2][1]])
+    return dStatedt
 
 
 # capacitance (units: nF)
@@ -132,7 +197,7 @@ th0 = 1.0
 th1 = 7.0
 
 # synaptic constants
-g12 = 0.35
+g12 = 0.35#80*0.35
 g23 = 0.27
 g32 = 0.215
 g31 = 0.203
@@ -145,6 +210,11 @@ S2 = 5.0 / 3.0
 
 V0  = -5.0
 dV0 = 5.0
+
+I = 40.0
+I_ext = np.array([0,
+                  I,
+                  I/4])# External stimulus (nA)
 
 # Vector equivalents to use later
 vA = np.array([vm,
@@ -159,13 +229,6 @@ tA0 = np.array([tm0,
 tA1 = np.array([tm1,
                 th1,
                 tn1])  # dim=(gating variables,)
-
-# dictionary needed to pass constants into ODEInt:
-constants_dict = {"vA":vA,
-                  "dvA":dvA,
-                  "tA0":tA0,
-                  "tA1":tA1}
-params = [constants_dict]
 
 # Internal ion channels
 g_internal_gating = np.array([gN,
@@ -185,72 +248,59 @@ E_rev_syn_matrix = np.array([[0    ,   Ereve,       0],
 
 
 
-def dStatedt(state, params):
-    # V1, V2, V3 = state[0:3]
-    # m1, m2, m2 = state[3:6]
-    # h1, h2, h2 = state[6:9]
-    # n1, n2, n2 = state[9:12]
-    vA = params[0]["vA"]
-    dvA = params[0]["dvA"]
-    tA0 = params[0]["tA0"]
-    tA1 = params[0]["tA1"]
+tau_syn_arr = np.array([[0    , tau1, 0],
+                        [0    , 0, tau1],
+                        [tau2,   tau2, 0]]) # (# neurons, # neurons)
+S_syn_arr = np.array([[0    , S1, 0],
+                        [0    , 0, S1],
+                        [S2,   S2, 0]]) # (# neurons, # neurons)
 
-    # Prepare for matrix multiplication later in function
-    V = state[0:3]
-    m = state[3:6]
-    h = state[6:9]
-    n = state[9:12]
-    # S = state[12:16] # S12, S23, S31, S32 = state[12:16]
-    S12, S23, S31, S32 = state[12:16]
-    synapse_arr = np.array([[0, S12, 0],
-                            [0, 0, S23],
-                            [S31, S32, 0]]) # dim index =(postsyn neuron index, presyn neuron index)
-
-    A = np.array([m,
-                  h,
-                  n]) # dim=(# gating variables,# neurons)
-
-    # Perform matrix multiplications
-    m3h = np.multiply(np.power(m, 3), h)
-    A_modulation = np.array([m3h,
-                            np.power(n,4),
-                            1.0]).transpose() # dim(3,3) or (# neurons, # ion channels)
-
-    # Voltages: dim(dVdt)=(# neurons,)
-    dVdt = calc_dVdt(V, A_modulation, g_internal_gating, E_rev_gate_matrix)
-    # Gating variables: dim(dAdt)=(3,)
-    dAdt = calc_dAdt(V,A,vA,dvA,tA0,tA1)
-    # Synapses: dim(dSdt)=(3,3)
-    tau_syn = np.array([tau1, tau2])
-    S_syn   = np.array([S1, S2])
-    dSdt = calc_dSdt(V, synapse_arr, V0, dV0, tau_syn, S_syn) # TODO: Arguments are not correctly set yet
-
-    dStatedt = np.ravel(np.array([dVdt, dAdt, dSdt])) # flatten array
-    return dStatedt
+# dictionary needed to pass constants into ODEInt:
+constants_dict = {"vA":vA,
+                  "dvA":dvA,
+                  "tA0":tA0,
+                  "tA1":tA1,
+                  "tau_syn_arr":tau_syn_arr,
+                  "S_syn_arr":S_syn_arr,
+                  "g_internal_gating" : g_internal_gating,
+                  "E_rev_gate_matrix" : E_rev_gate_matrix,
+                  "E_rev_syn_matrix" : E_rev_syn_matrix,
+                  "g_synapse_gating" : g_syn,
+                  "I_ext":I_ext}
+params = [constants_dict]
 
 # What do I want the end result to look like?
 t_start = 0.0 # ms
-t_stop = 100.0 # ms
+t_stop = 200.0 # ms
 dt = 0.02     # ms
 times_array = np.arange(start= t_start,stop= t_stop, step=dt)
 
-state_initial = np.zeros((16))
+state_initial = 0.1*np.ones((16))
+# state_initial[0:4] = 0.0 # voltages
+# state_initial[4:12] = 0.0 # gating variables
+# state_initial[12:16] = 0.0 # synapse variables
 
 # Solve system
-sol = odeint(dStatedt, times_array, state_initial, args=(params,))
-
+sol = scipy.integrate.solve_ivp(fun=dStatedt, t_span=[t_start,t_stop], y0=state_initial, t_eval=times_array, args=(params,))
 
 
 # Plotting
 fig = plt.figure(figsize=(20, 10))
 axs =  fig.add_subplot(111)
-axs.plot(times_array,sol.transpose())
+axs.plot(sol.t,(sol.y[0]).transpose(), label="V1")
+axs.plot(sol.t,(sol.y[1]).transpose(), label="V2")
+axs.plot(sol.t,(sol.y[2]).transpose(), label="V3")
+# axs.plot(sol.t,(sol.y[12]).transpose(), label="S12")
+
 axs.set_xlabel("Time (ms)")
 axs.set_ylabel("State variable")
+axs.legend()
 plt.title("Simulated 3 NaKL Network")
 fig.show()
 
-save_utilities.save_text(sol,"save","simulated_data/3NN/solution.txt")
+saved_data = np.vstack((sol.t,sol.y)).transpose()
+print(f"saved_data.shape:{saved_data.shape}")
+save_utilities.save_text(saved_data,"save","simulated_data/3NN/solution.txt")
 save_utilities.save_fig_with_makedir(figure=fig,
                                      save_location="simulated_data/3NN/solution.png")
 plt.close("all")
